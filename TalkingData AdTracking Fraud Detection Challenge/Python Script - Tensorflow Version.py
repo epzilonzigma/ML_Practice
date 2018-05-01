7fc9b72793030de1e0f015b882bd81417ca855ed
zigmaprime <36109957+zigmaprime@users.noreply.github.com>
2018-05-01 09:39:55 -0400

    Added Data Loads

--- TalkingData AdTracking Fraud Detection Challenge/Python Script - Tensorflow Version.py
+++ TalkingData AdTracking Fraud Detection Challenge/Python Script - Tensorflow Version.py
@@ -6,3 +6,17 @@ import pandas
 from sklearn.cross_validation import train_test_split
 import tensorflow as tf
 
+#create empty pandas dataframe to read CSV
+headings = ['ip', 'app','device', 'os', 'channel', 'click_time', 'attributed_time', 'is_attributed']
+data = pd.DataFrame(columns = headings)
+
+#read training data csv, append each row
+
+with open('C:/Users/Tony Cai/Documents/Ad Prediction/train_sample.csv', 'r') as csvfile:
+    dataset = csv.reader(csvfile)
+    for row in dataset:
+        if row[0] == "ip":
+            continue
+        else:
+            data = data.append(pd.Series(row, index=headings), ignore_index=True)
+
