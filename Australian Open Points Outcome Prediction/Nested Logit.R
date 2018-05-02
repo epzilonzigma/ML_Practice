cfbc734b79737237b3d4c1ad29ab1f821c01261c
zigmaprime <tony.zx.cai@gmail.com>
2018-05-01 23:34:54 -0400

    Nested Logit

--- Australian Open Points Outcome Prediction/Nested Logit.R
+++ Australian Open Points Outcome Prediction/Nested Logit.R
@@ -20,6 +20,7 @@ setwd("C:/Users/Tony Cai/Documents")
 mens.train <- read.csv("mens_train_file.csv", header = TRUE)
 womens.train <- read.csv("womens_train_file.csv", header = TRUE)
 model.spec <- read.csv("models.csv", header = FALSE)
+amodel.spec <- as.matrix(model.spec)
 
 #modify training data for mlogit package estimation
 
@@ -33,10 +34,37 @@ head(wt.data)
 log.likelihoodm <- c()
 log.likelihoodw <- c()
 
-for (i in c(1:3)){
-  model <- mlogit(formula(model.spec[i]),mt.data,nests=list(winnner = "W", error = c("UE","FE")), un.nest.el = TRUE)
-  log.likelihoodm[i] <- model$logLike
-  model <- mlogit(formula(model.spec[i]),wt.data,nests=list(winnner = "W", error = c("UE","FE")), un.nest.el = TRUE)
-  log.likelihoodw[i] <- model$logLike
+for (i in 1258675:nrow(amodel.spec)){
+  model <- mlogit(formula(amodel.spec[i]),mt.data,nests=list(winnner = "W", error = c("UE","FE")), un.nest.el = TRUE)
+  log.likelihoodm[i] <- model$logLik
+  model <- mlogit(formula(amodel.spec[i]),wt.data,nests=list(winnner = "W", error = c("UE","FE")), un.nest.el = TRUE)
+  log.likelihoodw[i] <- model$logLik
 }
 
+#exporting log.likelihood data into csv for model selection via computation and comparison of BIC
+
+log.like <- as.matrix(cbind(amodel.spec,log.likelihoodm,log.likelihoodw))
+write.csv(log.like, file = "loglikes.csv")
+
+#predicting values of test set
+
+### importing test dataset
+
+mens.test <- read.csv("mens_test_file.csv", header = TRUE)
+womens.test <- read.csv("womens_test_file.csv", header = TRUE)
+
+mens.tt <- mlogit.data(mens.test, choice = "outcome", shape = "wide")
+womens.tt <- mlogit.data(womens.test, choice = "outcome", shape = "wide")
+
+#estimate selected optimal model by BIC
+
+mens.p.model <- mlogit(outcome ~ 0 | serve+hitpoint+speed+net.clearance+distance.from.sideline+depth+outside.sideline+outside.baseline+player.distance.travelled+player.impact.depth+player.impact.distance.from.center+player.depth+opponent.depth+opponent.distance.from.center+previous.speed+previous.net.clearance+previous.distance.from.sideline+previous.depth+previous.hitpoint+server.is.impact.player, mt.data, nests =list(winnner = "W", error = c("UE","FE")), un.nest.el = TRUE)
+womens.p.model <- mlogit(outcome ~ 0 | rally+hitpoint+speed+net.clearance+distance.from.sideline+depth+outside.sideline+outside.baseline+player.distance.travelled+player.impact.depth+player.impact.distance.from.center+player.depth+player.distance.from.center+opponent.depth+opponent.distance.from.center+previous.speed+previous.net.clearance+previous.distance.from.sideline+previous.depth+previous.hitpoint+server.is.impact.player, wt.data, nests =list(winnner = "W", error = c("UE","FE")), un.nest.el = TRUE)
+
+#generate preditions for test sets and export as CSV
+
+mens.predictions <- predict(mens.p.model, newdata = mens.tt, returnData = TRUE)
+womens.predictions <- predict(womens.p.model, newdata = womens.tt, returnData = TRUE)
+
+write.csv(mens.predictions, "mens_predictions.csv")
+write.csv(womens.predictions, "womens_predictions.csv")
